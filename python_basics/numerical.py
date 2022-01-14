8ce816974d959ab49234fa8c8a488b65ccbb8451
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-15 00:20:46 +0900

    数値の扱い方

---
+++ python_basics/numerical.py
@@ -0,0 +1,32 @@
+print(2 + 2)
+# 4
+print(5 - 2)
+# 3
+print(17 / 3)
+# 5.666666666666667
+print(17 // 3)
+# 5
+print(17 % 3)
+# 2
+print(5 * 5)
+# 25
+print(5 ** 2)
+# 25
+
+x = 10
+y = 10
+print(x * y)
+# 100
+
+print(round(3.141415, 2))
+# 3.14
+
+
+import math
+
+result = math.sqrt(25)
+print(result)
+# 5.0
+
+# mathの説明書
+print(help(math))
