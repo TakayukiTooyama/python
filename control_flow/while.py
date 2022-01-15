9ee7b684fd75d47889f4027b3d654f4f4c03f339
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-16 02:02:08 +0900

    while文とinput関数（入力待ちとなる）

--- control_flow/while.py
+++ control_flow/while.py
@@ -73,3 +73,20 @@ else:
     print('done')
 # =>
 # 0
+
+
+# ⭐️input関数
+# 文字列の入力待ちになる
+while True:
+    word = input('Enter: ')
+    if word == 'ok':
+        break
+    print('next')
+
+# 数値の入力を待ちたい場合
+while True:
+    word = input('Enter: ')
+    num = int(word)
+    if num == 100:
+        break
+    print('next')
