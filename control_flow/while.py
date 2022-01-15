f6e60a5e2f18ec8930c10f661b7fa91716037d16
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-16 02:00:28 +0900

    continue文

--- control_flow/while.py
+++ control_flow/while.py
@@ -24,3 +24,22 @@ while True:
 # 2
 # 3
 # 4
+
+
+# continue文
+count = 0
+while True:
+    if count >= 5:
+        break
+
+    if count == 2:
+        count += 1
+        continue
+
+    print(count)
+    count += 1
+# =>
+# 0
+# 1
+# 3
+# 4
