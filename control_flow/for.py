8ead0bb9fddd0f60a317228f93274a72a4ec3c9e
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-16 02:10:28 +0900

    for文（list, str, break, continue）

---
+++ control_flow/for.py
@@ -0,0 +1,26 @@
+# for文
+some_list = [1, 2, 3, 4, 5]
+for i in some_list:
+    print(i)
+# =>
+# 1
+# 2
+# 3
+# 4
+# 5
+
+for s in 'abcde':
+    print(s)
+# =>
+# a
+# b
+# c
+# d
+# e
+
+for word in ['My', 'name', 'is', 'Mike']:
+    if word == 'name':
+        break
+    print(word)
+# =>
+# My
