1cfd4ca4362faec784edc951940d670d995d6258
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-16 00:04:39 +0900

    Notの使い所（in・isの使用時, boolean型の否定時）

--- control_flow/if.py
+++ control_flow/if.py
@@ -19,3 +19,27 @@ if a > 0:
     print('a is positive')
     if b > 0:
         print('b is positive')
+
+
+# Notの使い所
+x = 1
+y = [1, 2, 4]
+z = [2, 4]
+
+# ①inを使う時
+if x in y:
+    print('in') # in
+if x not in z:
+    print('Not in') # Not in
+
+# ②isを使う時
+is_empty = None
+if is_empty is not None:
+    print('None!!')
+
+# ⭐️③boolean型を否定する時
+is_ok = False
+if not is_ok:
+    print('False') # False
+else:
+    print('True')
