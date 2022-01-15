f944e2b5360c03e173bf14fb69f0b51396f6ce6e
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-16 02:01:18 +0900

    while else break文

--- control_flow/while.py
+++ control_flow/while.py
@@ -59,3 +59,17 @@ else:
 # 3
 # 4
 # done
+
+
+# while else break文
+count = 0
+while count < 5:
+    if count == 1:
+        break
+
+    print(count)
+    count += 1
+else:
+    print('done')
+# =>
+# 0
