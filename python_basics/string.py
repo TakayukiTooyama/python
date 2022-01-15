1c66483fc73bbfab613909c07fb15234c0aa22db
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-15 12:41:40 +0900

    改行と複数改行

--- python_basics/string.py
+++ python_basics/string.py
@@ -6,3 +6,26 @@ print("I don't s")
 print('I don\'t know')
 print('say "I don\'t know"')
 print("say \"I don\'t know\"")
+
+# 改行
+print('hello. \nHow are you?')
+# このままでは改行されてしまう
+print('C:\name\nname')
+# ⭐️rをつけることでrowデータにすることができる
+print(r'C:\name\nname')
+
+# 複数行の改行
+# ⭐️\を入れることで次の行から実行となる
+print("##############")
+print("""\
+line1
+line2
+line3\
+""")
+print("##############")
+# =>
+# ##############
+# line1
+# line2
+# line3
+# ##############
