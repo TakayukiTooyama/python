447effe50e5b00334e6793c2820267ae3d491da2
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-15 21:20:48 +0900

    辞書メソッド（keys, values, update, clear, in）

--- data_structure/dictionary.py
+++ data_structure/dictionary.py
@@ -21,3 +21,24 @@ d.pop('x')
 print(d) # {'y': 20, 'z': 200}
 del d['y']
 print(d) # {'z': 200}
+
+
+# 辞書メソッド
+d3 = {'x': 10, 'y': 20, 'z': 30}
+d4 = {'x': 100, 'o': 300, 'p':400}
+
+# キーと値の取得（keys, values）
+print(d3.keys()) # dict_keys(['x', 'y', 'z])
+print(d3.values()) # dict_values([10, 20, 30])
+
+# 辞書の更新（update）
+d3.update(d4)
+print(d3) # {'x': 100, 'y': 20, 'z': 30, 'o': 300, 'p': 400}
+
+# 辞書を空にする（clear）
+d3.clear()
+print(d3) # {}
+
+# キーがあるかどうか確かめる（in）
+print('p' in d4) # True
+print('j' in d4) # False
