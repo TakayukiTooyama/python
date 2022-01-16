8ebc5f1c0737e8259b40a271392e411bd82a0736
TakayukiTooyama <t.tooyama107@gmail.com>
2022-01-16 19:19:28 +0900

    デコレーター（関数を実行する前や後に何かしら機能を付け加えたい時）

--- control_flow/function.py
+++ control_flow/function.py
@@ -132,3 +132,31 @@ cal2 = circle_area_func(3.141592)
 
 print(cal1(10)) # 314.0
 print(cal2(10)) # 314.1592
+
+
+# デコレーター
+# 関数を実行する前や後に何かしら機能を付け加えたい時に使う
+def print_more(func):
+    def wrapper(*args, **kwargs):
+        print(func.__name__)
+        print(*args)
+        print(*kwargs)
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
+
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+    return wrapper
+
+@print_info
+@print_more
+def add_num(a, b):
+    return a + b
+
+r = add_num(10, 20)
+print(r)
